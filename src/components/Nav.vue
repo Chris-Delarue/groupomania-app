e55cf841a00f8de37cbc421376d72e5b63d98bd2
Chris Delarue <tophy.cd1@gmail.com>
2021-06-29 17:00:37 +0200

    create Nav.vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class='Nav'>
+        <router-link to='/'>
+             <h1>
+                <img src="/assets/images/icon-left-font.png" alt="Logo Groupomania">
+            </h1>
+        </router-link>
+
+        <nav>
+            <router-link to="/profil">
+            <div>Mon compte chez Groupomania</div>
+            </router-link>
+            <div id="logout-btn" @click = logout()>Se déconnecter</div>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name:  'Nav',
+    methods : {
+        logout(){
+            localStorage.removeItem('user');
+            location.href="/";
+        }
+    }
+}
+</script>
+
+<script>
+
+</script>
+
