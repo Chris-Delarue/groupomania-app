73d0e533ef022fe3787e2cdf53e2e02d256d8144
Chris Delarue <tophy.cd1@gmail.com>
2021-06-29 12:19:54 +0200

    create router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Post from '../views/Post.vue';
 
 Vue.use(VueRouter)
 
@@ -11,14 +12,22 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/post/:userId',
+    name: 'Post',
+    components: Post
+  },
+  {
+    path :'/signup',
+    name : 'Signup',
+    components : () => import('../views/Signup.vue')
+  },
+  {
+    path : '/profil',
+    name : 'profil',
+    components : () => import('../views/Profil.vue')
   }
 ]
+
 
 const router = new VueRouter({
   mode: 'history',
@@ -26,4 +35,4 @@ const router = new VueRouter({
   routes
 })
 
-export default router
+export default router;
